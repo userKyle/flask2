1b577784ee940e2ca6412d93082ed5ce08db8c82
userKyle <piquero.kylegerard@cec.edu.ph>
2025-05-13 00:09:19 +0800

    Thirdd Commit

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 def home():
     return 'Hello World'
 
-if __name__ == 'main':
- app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
