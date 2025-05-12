1bcc69201c666cc6869f059f37aaefd5373767ca
userKyle <piquero.kylegerard@cec.edu.ph>
2025-05-12 23:56:28 +0800

    Third Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello World'
+
+if __name__ == 'main':
+ app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
