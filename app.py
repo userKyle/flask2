11fd12b820a1c00973f3ad7cf0d661fe35101320
userKyle <piquero.kylegerard@cec.edu.ph>
2025-05-13 00:14:10 +0800

    Fourth Commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return 'Kyle Gerard Piquero - 3rd Year BSIT - System Integration and Architecture - Semi Final Exam'
 
 if __name__ == '__main__':
  port = int(os.environ.get("PORT", 5000))
